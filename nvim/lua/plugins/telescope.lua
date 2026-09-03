89bf107254ba224dbf017e36273ebcb99f8058e4
flori4n <florian.hamal03@gmail.com>
2026-09-03 19:20:27 +0200

    removed vim fugitive because telescope covers my most needed features

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,10 +7,13 @@ return {
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 		},
 		keys = {
-			{ "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Telescope find files" },
-			{ "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Telescope live grep" },
-			{ "<leader>fb", function() require("telescope.builtin").buffers({ sort_mru = true, ignore_current_buffer = true }) end, desc = "Telescope buffers" },
-			{ "<leader>fh", function() require("telescope.builtin").help_tags() end,  desc = "Telescope help tags" },
+			{ "<leader>ff",  function() require("telescope.builtin").find_files() end,                                               desc = "Telescope find files" },
+			{ "<leader>fg",  function() require("telescope.builtin").live_grep() end,                                                desc = "Telescope live grep" },
+			{ "<leader>fb",  function() require("telescope.builtin").buffers({ sort_mru = true, ignore_current_buffer = true }) end, desc = "Telescope buffers" },
+			{ "<leader>fh",  function() require("telescope.builtin").help_tags() end,                                                desc = "Telescope help tags" },
+			{ "<leader>gs",  function() require("telescope.builtin").git_status() end,                                               desc = "Git status" },
+			{ "<leader>gfh", function() require("telescope.builtin").git_bcommits() end,                                             desc = "Git file history" },
+			{ "<leader>gh",  function() require("telescope.builtin").git_commits() end,                                              desc = "Git repo history" },
 		},
 		opts = {
 			pickers = {
@@ -21,6 +24,7 @@ return {
 					},
 				},
 			},
+
 		},
 	},
 }
